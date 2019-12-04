a7ff7eac171ae932b954cf33d9e138447d9191fe
Ramanpreet6262 <ramanpreet6262@gmail.com>
2019-12-04 15:53:49 +0530

    added keyExtractor in FlatList

--- myproject/App.js
+++ myproject/App.js
@@ -3,19 +3,22 @@ import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
   const [people, setPeople] = useState([
-    { name: 'badri', key: '1' },
-    { name: 'raghav', key: '2' },
-    { name: 'rishabh', key: '3' },
-    { name: 'rakshit', key: '4' },
-    { name: 'ujjwal', key: '5' },
-    { name: 'jassi', key: '6' },
-    { name: 'raman', key: '7' },
-    { name: 'yuvek', key: '8' }
+    { name: 'badri', id: '1' },
+    { name: 'raghav', id: '2' },
+    { name: 'rishabh', id: '3' },
+    { name: 'rakshit', id: '4' },
+    { name: 'ujjwal', id: '5' },
+    { name: 'jassi', id: '6' },
+    { name: 'raman', id: '7' },
+    { name: 'yuvek', id: '8' }
   ]);
 
+  // key is needed for flatlist and if we don't have that in data then we can use keyextractor method...
+  // And in that we will tell which property of object is going to be used as key .....
   return (
     <View style={styles.container}>
       <FlatList
+        keyExtractor={item => item.id}
         data={people}
         renderItem={(
           { item } // The item here is destructured variable and it is the indiviaual item in the people array eg-- { name: 'badri', key: '1' }
@@ -24,6 +27,11 @@ export default function App() {
     </View>
   );
 }
+
+/* If we have a really large array of items, all of the items do not automatically load on to the 
+screen when we first render it....
+Only the first few loads and more will load as we scroll down WHEREAS when we use scrollview it renders every
+item from the beginning.....*/
 
 const styles = StyleSheet.create({
   container: {
